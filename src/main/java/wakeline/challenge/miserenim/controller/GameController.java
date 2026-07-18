ec80898027e9bf3b1495e59e35213f15344abc22
thelon <jps_90@gmx.de>
2026-07-18 11:58:44 +0200

    feat(game controller): implement endpoint for game creation and tests.

--- src/main/java/wakeline/challenge/miserenim/controller/GameController.java
+++ src/main/java/wakeline/challenge/miserenim/controller/GameController.java
@@ -1,4 +1,41 @@
 package wakeline.challenge.miserenim.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import wakeline.challenge.miserenim.dto.GameCreationResponse;
+import wakeline.challenge.miserenim.game.NimGame;
+import wakeline.challenge.miserenim.game.Player;
+import wakeline.challenge.miserenim.service.GameService;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/misere-nim")
+@Slf4j
 public class GameController {
+
+   private final GameService gameService;
+
+   @PostMapping("/create")
+   public ResponseEntity<GameCreationResponse> createGame(
+           @RequestParam int matches,
+           @RequestParam String player
+   ) {
+      log.info("Game creation request with {} matches and Player: {}", matches, player);
+
+      NimGame game = this.gameService.createNewGame(matches, Player.fromString(player.toLowerCase()));
+      GameCreationResponse response = new GameCreationResponse(
+              game.getId(),
+              game.getMatches(),
+              game.getCurrentPlayer().toString()
+      );
+
+      log.info("Game created with id: {}", game.getId());
+
+      return ResponseEntity.ok(response);
+   }
 }
---
+++ src/test/java/wakeline/challenge/miserenim/integration/GameControllerTest.java
@@ -0,0 +1,51 @@
+package wakeline.challenge.miserenim.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GameControllerTest {
+
+   @Autowired
+   private MockMvc mockMvc;
+
+   @Test
+   void testCreateGame() throws Exception {
+      mockMvc.perform(post("/api/v1/misere-nim/create")
+                      .param("matches", "5")
+                      .param("player", "human"))
+              .andExpect(status().isOk());
+   }
+
+   @Test
+   void testExceptionOnInsufficientNumberOfMatches() throws Exception {
+      mockMvc.perform(post("/api/v1/misere-nim/create")
+                      .param("matches", "0")
+                      .param("player", "human"))
+              .andExpect(status().isBadRequest());
+   }
+
+   @Test
+   void testExceptionOnIllegalPlayer() throws Exception {
+      mockMvc.perform(post("/api/v1/misere-nim/create")
+                      .param("matches", "0")
+                      .param("player", "Any"))
+              .andExpect(status().isBadRequest());
+   }
+
+   @Test
+   void testCaseInsensitivePlayerString() throws Exception {
+      mockMvc.perform(post("/api/v1/misere-nim/create")
+                      .param("matches", "2")
+                      .param("player", "Human"))
+              .andExpect(status().isOk());
+   }
+
+}
