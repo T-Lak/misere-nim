cbeccd9fb891bd06d5610fc829b8acf687b69eb8
thelon <jps_90@gmx.de>
2026-07-19 18:21:29 +0200

    chore(exception): add GameNotFoundException.

---
+++ src/main/java/wakeline/challenge/miserenim/exception/GameNotFoundException.java
@@ -0,0 +1,9 @@
+package wakeline.challenge.miserenim.exception;
+
+public class GameNotFoundException extends RuntimeException {
+
+   public GameNotFoundException(String message) {
+      super(message);
+   }
+
+}
