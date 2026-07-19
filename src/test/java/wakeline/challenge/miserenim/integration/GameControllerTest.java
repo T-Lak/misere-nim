1adbd150d984547fe41a059d7352275713e033cc
thelon <jps_90@gmx.de>
2026-07-19 18:21:06 +0200

    feat(move): implement endpoint to make a move
    - make computer move if human move was valid and game is not over
    - add unit and integration tests.

--- src/test/java/wakeline/challenge/miserenim/integration/GameControllerTest.java
+++ src/test/java/wakeline/challenge/miserenim/integration/GameControllerTest.java
@@ -5,7 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import tools.jackson.databind.ObjectMapper;
+import wakeline.challenge.miserenim.dto.GameCreationResponse;
+import wakeline.challenge.miserenim.dto.GameResponse;
+import wakeline.challenge.miserenim.game.GameStatus;
+import wakeline.challenge.miserenim.game.Player;
 
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -16,6 +23,8 @@ public class GameControllerTest {
    @Autowired
    private MockMvc mockMvc;
 
+   private static final ObjectMapper MAPPER = new ObjectMapper();
+
    @Test
    void testCreateGame() throws Exception {
       mockMvc.perform(post("/api/v1/misere-nim/create")
@@ -24,6 +33,13 @@ public class GameControllerTest {
                       .param("strategyType", "random")
               )
               .andExpect(status().isOk());
+   }
+
+   @Test
+   void testCreateGameWithComputerToStart() throws Exception {
+      GameCreationResponse gameCreationResponse = this.createGame("5", "computer", "stub");
+
+      assertEquals(4, gameCreationResponse.initialMatches());
    }
 
    @Test
@@ -54,6 +70,39 @@ public class GameControllerTest {
                       .param("strategyType", "random")
               )
               .andExpect(status().isOk());
+   }
+
+   @Test
+   void testHumanMoveProcessing() throws Exception {
+      GameCreationResponse gameCreationResponse = this.createGame("5", "human", "stub");
+
+      MvcResult moveResult = mockMvc.perform(post("/api/v1/misere-nim/" + gameCreationResponse.id() + "/move")
+                      .param("matches", "2")
+              )
+              .andExpect(status().isOk())
+              .andReturn();
+
+      String jsonResponseForMove = moveResult.getResponse().getContentAsString();
+      GameResponse gameResponse = MAPPER.readValue(jsonResponseForMove, GameResponse.class);
+
+      assertEquals(2, gameResponse.matchesLeft());
+      assertEquals(2, gameResponse.moveHistory().size());
+      assertEquals(Player.HUMAN, gameResponse.currentPlayer());
+      assertEquals(GameStatus.IN_PROGRESS, gameResponse.status());
+   }
+
+   private GameCreationResponse createGame(String matches, String player, String strategy) throws Exception {
+      MvcResult creationResult = mockMvc.perform(post("/api/v1/misere-nim/create")
+                      .param("matches", matches)
+                      .param("player", player)
+                      .param("strategyType", strategy)
+              )
+              .andExpect(status().isOk())
+              .andReturn();
+
+      String jsonResponseForCreation = creationResult.getResponse().getContentAsString();
+
+      return MAPPER.readValue(jsonResponseForCreation, GameCreationResponse.class);
    }
 
 }
--- src/test/java/wakeline/challenge/miserenim/unit/GameServiceTest.java
+++ src/test/java/wakeline/challenge/miserenim/unit/GameServiceTest.java
@@ -1,13 +1,16 @@
 package wakeline.challenge.miserenim.unit;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import wakeline.challenge.miserenim.exception.GameNotFoundException;
 import wakeline.challenge.miserenim.exception.InvalidMoveException;
+import wakeline.challenge.miserenim.game.GameStatus;
 import wakeline.challenge.miserenim.game.NimGame;
+import wakeline.challenge.miserenim.game.NimGameFactory;
 import wakeline.challenge.miserenim.game.Player;
 import wakeline.challenge.miserenim.service.GameService;
-import wakeline.challenge.miserenim.strategy.StrategyType;
 
 import static org.junit.Assert.*;
 
@@ -27,11 +30,36 @@ public class GameServiceTest {
    }
 
    @Test
-   void shouldExceptionOnGameCreationWithInsufficientNumberOfMatches() {
+   void testExceptionOnGameCreationWithInsufficientNumberOfMatches() {
       assertThrows(InvalidMoveException.class, () -> gameService.createNewGame(
               1,
               Player.HUMAN,
               "random"
       ));
    }
+
+   @Test
+   void shouldDecreaseMatchesCountWhenComputerStarts() {
+      NimGame game = gameService.createNewGame(5, Player.COMPUTER, "stub");
+
+      assertEquals(4, game.getMatches());
+   }
+
+   @Test
+   void testMakeHumanMove() {
+      NimGame game = gameService.createNewGame(5, Player.HUMAN, "stub");
+
+      gameService.processHumanMove(game.getId(), 3);
+
+      assertEquals(1, game.getMatches());
+      assertSame(GameStatus.FINISHED, game.getStatus());
+   }
+
+   @Test
+   void testGameNotFoundExceptionOnNonExistingId() {
+      NimGame game = gameService.createNewGame(5, Player.HUMAN, "stub");
+
+      assertThrows(GameNotFoundException.class, () -> gameService.processHumanMove("1", 3));
+   }
+
 }
