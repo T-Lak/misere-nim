ba3a1c7312cfabc4669cef192c3ba2362a0a9e8a
T-Lak <jps_90@gmx.de>
2026-08-20 23:17:20 +0200

    chore: move classes from unit to integration

--- src/test/java/challenge/miserenim/integration/GameServiceTest.java
+++ src/test/java/challenge/miserenim/integration/GameServiceTest.java
@@ -1,4 +1,4 @@
-package challenge.miserenim.unit;
+package challenge.miserenim.integration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/java/challenge/miserenim/integration/InMemoryGameRepositoryTest.java
+++ src/test/java/challenge/miserenim/integration/InMemoryGameRepositoryTest.java
@@ -1,4 +1,4 @@
-package challenge.miserenim.unit;
+package challenge.miserenim.integration;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
