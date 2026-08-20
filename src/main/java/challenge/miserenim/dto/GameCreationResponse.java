19d747138cd5093c22587ebfe34cc40071f73395
T-Lak <jps_90@gmx.de>
2026-08-20 22:55:42 +0200

    fix(api): use consistent camelCase for playerToStart

--- src/main/java/challenge/miserenim/dto/GameCreationResponse.java
+++ src/main/java/challenge/miserenim/dto/GameCreationResponse.java
@@ -8,7 +8,7 @@ import java.util.List;
 public record GameCreationResponse(
    String id,
    int initialMatches,
-   String PlayerToStart,
+   String playerToStart,
    StrategyType computerStrategy,
    List<MoveLog> moveHistory
 ) {}
